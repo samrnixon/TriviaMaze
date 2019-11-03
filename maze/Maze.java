89a8cad869e6bf3e93371e7d03d2916f0b27fa9e
Devin <dkramer2@eagles.ewu.edu>
2019-11-03 09:32:50 -0800

    Added constructor

--- maze/Maze.java
+++ maze/Maze.java
@@ -5,5 +5,12 @@ public class Maze {
 	private Room[][] rooms;
 	private Player player;
 	private MazeBuilder builder;
+	
+	public Maze(int rows, int columns, MazeBuilder builder, String name, int age)
+	{
+		this.player = new Player(name, age);
+		this.rooms = new Room[rows][columns];
+		this.builder = builder;
+	}
 
 }
