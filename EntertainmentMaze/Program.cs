177c56828acffbfa59792d2e5a3565ca63f58aad
Samuel Nixon <samnixon117@gmail.com>
2019-12-05 22:28:46 -0800

    Added better movement in maze, back and forth.

--- EntertainmentMaze/Program.cs
+++ EntertainmentMaze/Program.cs
@@ -1,11 +1,14 @@
 using EntertainmentMaze.Database;
 using EntertainmentMaze.maze;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace EntertainmentMaze
 {
@@ -13,9 +16,10 @@ namespace EntertainmentMaze
     {
         internal static Player newPlayer;
         public static Maze playerMaze;
-        public static string SaveFile = "GameSaves.txt";
+        public static string SaveFile = "GameSaves.json";
         internal static IFormatter formatter = new BinaryFormatter();
         private static string PlayResumeMenuOption = "Play";
+        private static Stream _Source;
 
         public static void Main()
         {
@@ -66,7 +70,7 @@ namespace EntertainmentMaze
                         InGameMenu();
                         break;
                     case 2:
-                        LoadGame();
+                        //LoadGame();
                         break;
                     default:
                         return;
@@ -101,28 +105,19 @@ namespace EntertainmentMaze
                 switch (selection)
                 {
                     case 1:
-                        //Ask question, if correct then move North
-                        //playerMaze.MoveHero("N");
-                        MovementAttempt("N");
+                        PlayerControl.MovementAttempt(playerMaze, "N");
                         break;
                     case 2:
-                        //Ask question, if correct then move East
-                        //playerMaze.MoveHero("E");
-                        MovementAttempt("E");
+                        PlayerControl.MovementAttempt(playerMaze, "E");
                         break;
                     case 3:
-                        //Ask question, if correct then move South
-                        //playerMaze.MoveHero("S");
-                        MovementAttempt("S");
+                        PlayerControl.MovementAttempt(playerMaze, "S");
                         break;
                     case 4:
-                        //Ask question, if correct then move West
-                        //playerMaze.MoveHero("W");
-                        MovementAttempt("W");
+                        PlayerControl.MovementAttempt(playerMaze, "W");
                         break;
                     case 5:
-                        //Save game
-                        SaveGame();
+                        //SaveGame(playerMaze);
                         break;
                     case 6:
                         playerMaze.DisplayHeroLocation();
@@ -134,129 +129,48 @@ namespace EntertainmentMaze
             }
         }
 
-        private static void SaveGame()
+        public static Maze LoadGame()
         {
-            Program.SerializeItem(SaveFile, formatter);
+            Source.Position = 0;
+            using var reader = new StreamReader(Source, leaveOpen: true);
+            string jsonString = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<Maze>(jsonString);
         }
 
-        private static void LoadGame()
+        public static void SaveGame(Maze maze)
         {
-            //Program.DeserializeItem(SaveFile, formatter);
-        }
+            var serializer = new JsonSerializer();
 
-        public static void SerializeItem(string fileName, IFormatter formatter)
-        {
-            FileStream s = new FileStream(fileName, FileMode.Create);
-            formatter.Serialize(s, playerMaze);
-            s.Close();
-        }
-
-
-        public static void DeserializeItem(string fileName, IFormatter formatter)
-        {
-            FileStream s = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            playerMaze = (Maze)formatter.Deserialize(s);
-        }
-
-        private static void MovementAttempt(string movementDirection)
-        {
-            if(movementDirection == "N")
+            using (var sw = new StreamWriter(SaveFile))
+            using (JsonWriter writer = new JsonTextWriter(sw))
             {
-                if (playerMaze.GetLocation().NorthDoor.GetDoorStatus() is true)
-                {
-                    Console.WriteLine("This Door is locked! You cannot go through here.");
-                    return;
-                }
-                else
-                {
-                    if(QuestionAttempt(playerMaze.GetLocation().NorthDoor.DisplayQuestion(), playerMaze.GetLocation().NorthDoor.DisplayAnswer()) is true)
-                    {
-                        playerMaze.MoveHero("N");
-                    }
-                    else
-                    {
-                        playerMaze.GetLocation().NorthDoor.LockDoor();
-                    }
-                }
-
-            }
-            if (movementDirection == "E")
-            {
-                if (playerMaze.GetLocation().EastDoor.GetDoorStatus() is true)
-                {
-                    Console.WriteLine("This Door is locked! You cannot go through here.");
-                    return;
-                }
-                else
-                {
-                    if(QuestionAttempt(playerMaze.GetLocation().EastDoor.DisplayQuestion(), playerMaze.GetLocation().EastDoor.DisplayAnswer()) is true)
-                    {
-                        playerMaze.MoveHero("E");
-                    }
-                    else
-                    {
-                        playerMaze.GetLocation().EastDoor.LockDoor();
-                    }
-                }
-            }
-            if (movementDirection == "S")
-            {
-                if (playerMaze.GetLocation().SouthDoor.GetDoorStatus() is true)
-                {
-                    Console.WriteLine("This Door is locked! You cannot go through here.");
-                    return;
-                }
-                else
-                {
-                    if(QuestionAttempt(playerMaze.GetLocation().SouthDoor.DisplayQuestion(), playerMaze.GetLocation().SouthDoor.DisplayAnswer()) is true)
-                    {
-                        playerMaze.MoveHero("S");
-                    }
-                    else
-                    {
-                        playerMaze.GetLocation().SouthDoor.LockDoor();
-                    }
-                }
-            }
-            if (movementDirection == "W")
-            {
-                if (playerMaze.GetLocation().WestDoor.GetDoorStatus() is true)
-                {
-                    Console.WriteLine("This Door is locked! You cannot go through here.");
-                    return;
-                }
-                else
-                {
-                    if(QuestionAttempt(playerMaze.GetLocation().WestDoor.DisplayQuestion(), playerMaze.GetLocation().WestDoor.DisplayAnswer()) is true)
-                    {
-                        playerMaze.MoveHero("W");
-                    }
-                    else
-                    {
-                        playerMaze.GetLocation().WestDoor.LockDoor();
-                    }
-                }
+                serializer.Serialize(writer, maze);
             }
         }
-
-        private static bool QuestionAttempt(string question, string answer)
+/*
+        public static Maze DataLoader(string filePath)
         {
-            string attemptedAnswer = "";
-            Console.WriteLine("Here is the question to open the door:\n");
-            Console.WriteLine(question);
+            var serializer = new JsonSerializer();
 
-            attemptedAnswer = Console.ReadLine();
+            using (var sw = new StreamReader(filePath))
+            using (var reader = new JsonTextReader(sw))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }*/
 
-            if (answer.ToLowerInvariant().Equals(attemptedAnswer.ToLowerInvariant()))
-            {
-                Console.WriteLine("Correct Answer! You move to the next room...");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect! The Door locks...");
-                return false;
-            }
-        }
+        /*        public static void SerializeItem(string fileName, IFormatter formatter)
+                {
+                    FileStream s = new FileStream(fileName, FileMode.Create);
+                    formatter.Serialize(s, playerMaze);
+                    s.Close();
+                }
+
+
+                public static void DeserializeItem(string fileName, IFormatter formatter)
+                {
+                    FileStream s = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    playerMaze = (Maze)formatter.Deserialize(s);
+                }*/
     }
 }
---
+++ EntertainmentMaze/maze/PlayerControl.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntertainmentMaze.maze
+{
+    class PlayerControl
+    {
+        public static void MovementAttempt(Maze playerMaze, string movementDirection)
+        {
+            switch (movementDirection)
+            {
+                case "N":
+                    if ((playerMaze.GetLocation().NorthDoor.GetDoorStatus()))
+                    {
+                        Console.WriteLine("This Door is locked! You cannot go through here.");
+                        return;
+                    }
+                    else if (playerMaze.GetLocation().NorthDoor.GetDoorOpenedStatus() is true)
+                    {
+                        playerMaze.MoveHero("N");
+                    }
+                    else
+                    {
+                        if (QuestionAttempt(playerMaze.GetLocation().NorthDoor.DisplayQuestion(), playerMaze.GetLocation().NorthDoor.DisplayAnswer()) is true)
+                        {
+                            playerMaze.GetLocation().NorthDoor.OpenDoor();
+                            playerMaze.MoveHero("N");
+                            playerMaze.GetLocation().SouthDoor.OpenDoor();
+                        }
+                        else
+                        {
+                            playerMaze.GetLocation().NorthDoor.LockDoor();
+                        }
+                    }
+                    return;
+                case "E":
+                    if (playerMaze.GetLocation().EastDoor.GetDoorStatus())
+                    {
+                        Console.WriteLine("This Door is locked! You cannot go through here.");
+                        return;
+                    }
+                    else if (playerMaze.GetLocation().EastDoor.GetDoorOpenedStatus() is true)
+                    {
+                        playerMaze.MoveHero("E");
+                    }
+                    else
+                    {
+                        if (QuestionAttempt(playerMaze.GetLocation().EastDoor.DisplayQuestion(), playerMaze.GetLocation().EastDoor.DisplayAnswer()) is true)
+                        {
+                            playerMaze.GetLocation().EastDoor.OpenDoor();
+                            playerMaze.MoveHero("E");
+                            playerMaze.GetLocation().WestDoor.OpenDoor();
+                        }
+                        else
+                        {
+                            playerMaze.GetLocation().EastDoor.LockDoor();
+                        }
+                    }
+                    return;
+                case "S":
+                    if (playerMaze.GetLocation().SouthDoor.GetDoorStatus() is true)
+                    {
+                        Console.WriteLine("This Door is locked! You cannot go through here.");
+                        return;
+                    }
+                    else if (playerMaze.GetLocation().SouthDoor.GetDoorOpenedStatus() is true)
+                    {
+                        playerMaze.MoveHero("S");
+                    }
+                    else
+                    {
+                        if (QuestionAttempt(playerMaze.GetLocation().SouthDoor.DisplayQuestion(), playerMaze.GetLocation().SouthDoor.DisplayAnswer()) is true)
+                        {
+                            playerMaze.GetLocation().SouthDoor.OpenDoor();
+                            playerMaze.MoveHero("S");
+                            playerMaze.GetLocation().NorthDoor.OpenDoor();
+                        }
+                        else
+                        {
+                            playerMaze.GetLocation().SouthDoor.LockDoor();
+                        }
+                    }
+                    return;
+                case "W":
+                    if (playerMaze.GetLocation().WestDoor.GetDoorStatus() is true)
+                    {
+                        Console.WriteLine("This Door is locked! You cannot go through here.");
+                        return;
+                    }
+                    else if(playerMaze.GetLocation().WestDoor.GetDoorOpenedStatus() is true)
+                    {
+                        playerMaze.MoveHero("W");
+                    }
+                    else
+                    {
+                        if (QuestionAttempt(playerMaze.GetLocation().WestDoor.DisplayQuestion(), playerMaze.GetLocation().WestDoor.DisplayAnswer()) is true)
+                        {
+                            playerMaze.GetLocation().WestDoor.OpenDoor();
+                            playerMaze.MoveHero("W");
+                            playerMaze.GetLocation().EastDoor.OpenDoor();
+                        }
+                        else
+                        {
+                            playerMaze.GetLocation().WestDoor.LockDoor();
+                        }
+                    }
+                    return;
+            }
+        }
+
+        public static bool QuestionAttempt(string question, string answer)
+        {
+            string attemptedAnswer = "";
+            Console.WriteLine("Here is the question to open the door:\n");
+            Console.WriteLine(question);
+
+            attemptedAnswer = Console.ReadLine();
+
+            if (answer.ToLowerInvariant().Equals(attemptedAnswer.ToLowerInvariant()))
+            {
+                Console.WriteLine("Correct Answer! You move to the next room...");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect! The Door locks...");
+                return false;
+            }
+        }
+    }
+}
